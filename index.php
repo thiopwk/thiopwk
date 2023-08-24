5d7d56f4e00f51ebabdae2c869aa0035b57211ea
thiopwk <thioputra0407@gmail.com>
2023-08-24 11:55:46 +0700

    hallo

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+  <div class="conteiner text-center">
+    <div class="row align-items-start">
+      <div class="col-xs-6-col-sm-3 col-md-4">
+        <img src="Screenshot (7).png" class="img-fluid img-thumbnail" alt="...">
+      <div class="col-xs-6-col-sm-3 col-md-4">
+        <img src="Screenshot (7).png" class="img-fluid rounded float-start" alt="...">
+      <div class="col-xs-6-col-sm-3 col-md-4">
+        <img src="Screenshot (7).png" class="img-fluid rounded float-start" alt="...">
+      <div class="col-xs-6-col-sm-3 col-md-4">
+        <img src="Screenshot (7).png" class="img-fluid rounded float-start" alt="...">
+      </div>
+    </div>
+  </div>  
+
+<script src="/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
